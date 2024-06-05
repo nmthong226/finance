3f207640185216952bfbd88e049c0dbae8ede796
nmthong226 <nmthong226@gmail.com>
2024-06-05 22:15:33 +0700

    add chart monitor

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,11 +1,22 @@
 import {
+    Select,
+    SelectTrigger,
+    SelectContent,
+    SelectValue,
+    SelectItem,
+} from "@/components/ui/select";
+import {
     Card,
     CardContent,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
-import { FileSearch } from "lucide-react";
+} from "@/components/ui/card";
+import { AreaChartIcon, BarChart2Icon, FileSearch, LineChartIcon } from "lucide-react";
 import { AreaVariant } from "@/components/area-variant";
+import { BarVariant } from "@/components/bar-variant";
+import { useState } from "react";
+import { AreaChart } from "recharts";
+import { LineVariant } from "./line-variant";
 
 type Props = {
     data?: {
@@ -15,24 +26,66 @@ type Props = {
     }[];
 };
 
-export const Chart = ({data = []}: Props) => {
+export const Chart = ({ data = [] }: Props) => {
+    const [chartType, setCharType] = useState("area");
+    const onTypeChange = (type: string) => {
+        setCharType(type);
+    }
     return (
         <Card className="border-none drop-shadow-sm">
             <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
                 <CardTitle className="text-xl line-clamp-1">
                     Transactions
                 </CardTitle>
+                <Select
+                    defaultValue={chartType}
+                    onValueChange={onTypeChange}
+                >
+                    <SelectTrigger className="lg:w-auto h-9 rounded-md px-3">
+                        <SelectValue placeholder="Chart Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="area">
+                            <div className="flex items-center">
+                                <AreaChartIcon className="size-4 mr-2 shrink-0"/>
+                                <p className="line-clamp-1">
+                                    Area chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="bar">
+                            <div className="flex items-center">
+                                <BarChart2Icon className="size-4 mr-2 shrink-0"/>
+                                <p className="line-clamp-1">
+                                    Bar chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                        <SelectItem value="line">
+                            <div className="flex items-center">
+                                <LineChartIcon className="size-4 mr-2 shrink-0"/>
+                                <p className="line-clamp-1">
+                                    Line chart
+                                </p>
+                            </div>
+                        </SelectItem>
+                    </SelectContent>
+                </Select>
             </CardHeader>
             <CardContent>
                 {data.length === 0 ? (
                     <div className="flex flex-col gap-y-4 items-center justify-center h-[350px] w-full">
-                        <FileSearch className="size-6 text-muted-foreground"/>
+                        <FileSearch className="size-6 text-muted-foreground" />
                         <p className="text-muted-foreground text-sm">
                             No data for this period
                         </p>
                     </div>
                 ) : (
-                    <AreaVariant data={data} />
+                    <>
+                        {chartType === "area" && <AreaVariant data={data} />}
+                        {chartType === "bar" && <BarVariant data={data} />}
+                        {chartType === "line" && <LineVariant data={data} />}
+                    </>
                 )}
             </CardContent>
         </Card>
