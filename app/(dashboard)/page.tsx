5bc811e37c113fc2b31ebf9fc01b25123a5cda47
nmthong226 <nmthong226@gmail.com>
2024-06-05 20:43:56 +0700

    add chart & count effect

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { DataCharts } from "@/components/data-charts";
 import DataGrid from "@/components/data-grid";
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
@@ -8,6 +9,7 @@ export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
+      <DataCharts />
     </div>
   )
 }
