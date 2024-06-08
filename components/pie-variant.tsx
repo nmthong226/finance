e74fd8280e78e63c2b2f3b254d0dbd69d0ee9f91
nmthong226 <nmthong226@gmail.com>
2024-06-08 23:44:13 +0700

    mini change

--- components/pie-variant.tsx
+++ components/pie-variant.tsx
@@ -34,6 +34,8 @@ export const PieVariant = ({ data }: Props) => {
                                         className="flex items-center space-x-2"
                                     >
                                         <span
+                                            className="size-2 rounded-full"
+                                            style={{ backgroundColor: entry.color}}
                                         />
                                     </li>
                                 ))}
