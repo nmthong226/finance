37b8baf8b03baf4bf7eb2835e0b6b61dc5e892bd
nmthong226 <nmthong226@gmail.com>
2024-06-13 23:19:21 +0700

    delete unused things

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { DataCharts } from "@/components/data-charts";
 import DataGrid from "@/components/data-grid";
-import { Button } from "@/components/ui/button";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 
 export default function DashboardPage() {
-  const {onOpen} = useNewAccount();
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <DataGrid />
