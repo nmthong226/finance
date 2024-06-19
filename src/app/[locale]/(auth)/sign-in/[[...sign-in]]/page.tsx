5dac110e7b0139b0d0541ee2f814dca9bb753e52
nmthong226 <nmthong226@gmail.com>
2024-06-19 09:36:27 +0700

    fix bug: locale authentication in authen

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import { Loader2 } from "lucide-react";
 import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { useLocale } from "next-intl";
 
 export default function Page() {
+  const locale = useLocale();
+  const path = locale === "vn" ? "/vn/sign-in" : "/en/sign-in";
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignIn path="/sign-in" />
+            <SignIn path={path} />
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground"/>
