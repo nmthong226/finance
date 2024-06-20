367a54090b616ab825db7c61ca3f53e1b7184181
nmthong226 <nmthong226@gmail.com>
2024-06-20 16:53:26 +0700

    add locale in sign-up page

--- src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/[locale]/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -5,13 +5,14 @@ import { useLocale } from "next-intl";
 
 export default function Page() {
   const locale = useLocale();
-  const path = locale === "vn" ? "/vn/sign-in" : "/en/sign-in";
+  const pathSignUp = locale === "vn" ? "/vn/sign-up" : "/en/sign-up";
+  const pathSignIn = locale === "vn" ? "/vn/sign-in" : "/en/sign-in";
   return (
     <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
       <div className="h-full lg:flex flex-col items-center justify-center px-4">
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignIn path={path} />
+            <SignIn path={pathSignIn} signUpUrl={pathSignUp}/>
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground"/>
