c5fcda07467421d1b2d34dc5a2e59905f2818fe6
nmthong226 <nmthong226@gmail.com>
2024-06-21 13:29:08 +0700

    add locale for date in date picker

--- components/date-picker.tsx
+++ components/date-picker.tsx
@@ -11,6 +11,8 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import { useLocale, useTranslations } from "next-intl";
+import { enUS, vi } from "date-fns/locale";
 
 type Props = {
     value?: Date;
@@ -23,6 +25,9 @@ export const DatePicker = ({
     onChange,
     disabled,
 }: Props) => {
+    const t = useTranslations('TransactionHistory');
+    const locale = useLocale();
+    const calendarLanguage = locale === "vn" ? vi : enUS;
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -32,7 +37,7 @@ export const DatePicker = ({
                     className={cn("w-full justify-start text-left font-normal", !value && "text-muted-foreground")}
                 >
                     <CalendarIcon className="size-4 mr-2" />
-                    {value ? format(value, "PPP") : <span>Pick a date</span>}
+                    {value ? format(value, "PPP", {locale: calendarLanguage}) : <span>{t('date-select')}</span>}
                 </Button>
             </PopoverTrigger>
             <PopoverContent>
@@ -42,6 +47,7 @@ export const DatePicker = ({
                     onSelect={onChange}
                     disabled={disabled}
                     initialFocus
+                    locale={calendarLanguage}
                 />
             </PopoverContent>
         </Popover>
