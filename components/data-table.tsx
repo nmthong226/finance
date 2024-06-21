4e29f91333aed73426b1028ce9178432569a4afc
nmthong226 <nmthong226@gmail.com>
2024-06-21 13:28:51 +0700

    fix bug in id column

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -76,9 +76,9 @@ export function DataTable<TData, TValue>({
             <div className="flex items-center py-4">
                 <Input
                     placeholder={`${t('filter')} ${filterKey}...`}
-                    value={(table.getColumn(filterKey)?.getFilterValue() as string) ?? ""}
+                    value={(table.getColumn("payee")?.getFilterValue() as string) ?? ""}
                     onChange={(event) =>
-                        table.getColumn(filterKey)?.setFilterValue(event.target.value)
+                        table.getColumn("payee")?.setFilterValue(event.target.value)
                     }
                     className="max-w-sm"
                 />
