c74d33bcf072bd509ce53f920a092ae16f5fb1c7
nmthong226 <nmthong226@gmail.com>
2024-06-24 23:12:54 +0700

    fix bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 import createMiddleware from 'next-intl/middleware';
- 
+import { NextResponse } from "next/server";
+
 const intlMiddleware = createMiddleware({
   locales: ['en', 'vn'],
   defaultLocale: 'en'
@@ -9,7 +10,13 @@ const intlMiddleware = createMiddleware({
 const isPublicRoute = createRouteMatcher(['/:locale/sign-in(.*)', '/:locale/sign-up(.*)']);
  
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) auth().protect();
+  const { pathname } = req.nextUrl;
+  const locale = pathname.split('/')[1] || 'en';
+  const {userId} = auth();
+  if (!userId && !isPublicRoute(req)) {
+    const redirectUrl = new URL(`/${locale}/sign-in`, req.url);
+    return NextResponse.redirect(redirectUrl);
+  }
   return intlMiddleware(req);
 });
  
